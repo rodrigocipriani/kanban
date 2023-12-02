93fae12d6223f62d5b876263b28320c8cc671bb7
Rodrigo Cipriani da Rosa <rodrigocipriani@gmail.com>
2023-12-02 15:38:24 -0300

    script to check imports

--- showMorph.js
+++ showMorph.js
@@ -11,12 +11,44 @@ const sourceFiles = project.getSourceFiles()
 
 // Iterate over the source files and extract dependencies
 sourceFiles.forEach((sourceFile) => {
-  console.log('File: ', sourceFile.getFilePath())
+  const sourceFilePath = sourceFile.getFilePath().toString()
+  console.log('File: ', sourceFilePath)
 
   // Get all import declarations in the file
   const imports = sourceFile.getImportDeclarations()
 
   imports.forEach((importDeclaration) => {
-    console.log('  Imports: ', importDeclaration.getModuleSpecifier().getText())
+    const sourceFileImports = importDeclaration.getModuleSpecifier().getText()
+    // console.log('  Imports: ', sourceFileImports)
+
+    if (sourceFilePath.includes('/src/frontend')) {
+      if (sourceFileImports.includes('@/backend')) {
+        console.error(
+          'ERROR: Importing backend from frontend',
+          sourceFileImports
+        )
+      }
+    }
+
+    if (sourceFilePath.includes('/src/backend')) {
+      if (sourceFileImports.includes('@/frontend')) {
+        console.error(
+          'ERROR: Importing backend from frontend',
+          sourceFileImports
+        )
+      }
+    }
+
+    if (sourceFilePath.includes('/src/shared')) {
+      if (
+        sourceFileImports.includes('@/frontend') ||
+        sourceFileImports.includes('@/backend')
+      ) {
+        console.error(
+          'ERROR: Importing shared from frontend or backend',
+          sourceFileImports
+        )
+      }
+    }
   })
 })
